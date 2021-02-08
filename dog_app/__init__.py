dd7e72c0e0be338f7da766fbea78181664ec7267
Magali Semeria <magali.semeria@gmail.com>
2021-02-08 22:14:15 +0100

    Clean up dead code

--- dog_app/__init__.py
+++ dog_app/__init__.py
@@ -1,4 +1,3 @@
-import glob
 import os
 from flask import Flask, flash, request, redirect, url_for, send_from_directory, render_template
 from werkzeug.utils import secure_filename
@@ -44,22 +43,21 @@ def upload_file():
     """
         Upload a file to upload folder
     """
-    if request.method == 'POST':
-        delete_uploads()
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('index'))
+    delete_uploads()
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('index'))
 
 
 @app.route('/uploads/<filename>')
