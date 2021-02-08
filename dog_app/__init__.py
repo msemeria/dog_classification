1ab4a51e87a757c98b0290f9701fdda02f5f2b4d
Magali Semeria <magali.semeria@gmail.com>
2021-02-08 21:01:50 +0100

    Deleted obsolete html template in init file

--- dog_app/__init__.py
+++ dog_app/__init__.py
@@ -39,15 +39,6 @@ def upload_file():
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('index'))
-    # return '''
-    # <!doctype html>
-    # <title>Upload new File</title>
-    # <h1>Upload new File</h1>
-    # <form method=post enctype=multipart/form-data>
-    #   <input type=file name=file>
-    #   <input type=submit value=Upload>
-    # </form>
-    # '''
 
 
 @app.route('/uploads/<filename>')
