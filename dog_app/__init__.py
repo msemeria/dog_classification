5ebac43add228fe5cdf309dfc18df85970af9648
Magali Semeria <magali.semeria@gmail.com>
2021-02-08 15:50:41 +0100

    Upload file by clicking on a button and display all files in upload folder

--- dog_app/__init__.py
+++ dog_app/__init__.py
@@ -1,36 +1,58 @@
 import os
-
-from flask import Flask, render_template, request, redirect, url_for
-from flask_dropzone import Dropzone
+from flask import Flask, flash, request, redirect, url_for, send_from_directory, render_template
+from werkzeug.utils import secure_filename
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+UPLOAD_FOLDER = os.path.join(basedir, 'uploads')
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
-
-app.config.update(
-    UPLOADED_PATH=os.path.join(basedir, 'uploads'),
-    # Flask-Dropzone config:
-    DROPZONE_ALLOWED_FILE_TYPE='image',
-    DROPZONE_MAX_FILE_SIZE=3,
-    DROPZONE_MAX_FILES=1,
-    # DROPZONE_REDIRECT_VIEW='result'
-)
-
-dropzone = Dropzone(app)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 3 * 1024 * 1024
 
 
-@app.route('/result/<img>', methods=['POST', 'GET'], )
-def result(img):
-    return render_template('result.html')
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/', methods=['POST', 'GET'])
-def upload():
+@app.route('/')
+def index():
+    files = os.listdir(app.config['UPLOAD_FOLDER'])
+    return render_template('index.html', files=files)
+
+
+@app.route('/', methods=['POST'])
+def upload_file():
     if request.method == 'POST':
-        f = request.files.get('file')
-        file_path = os.path.join(app.config['UPLOADED_PATH'], f.filename)
-        f.save(file_path)
-    return render_template('index.html')
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('index'))
+    # return '''
+    # <!doctype html>
+    # <title>Upload new File</title>
+    # <h1>Upload new File</h1>
+    # <form method=post enctype=multipart/form-data>
+    #   <input type=file name=file>
+    #   <input type=submit value=Upload>
+    # </form>
+    # '''
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 
 if __name__ == '__main__':
