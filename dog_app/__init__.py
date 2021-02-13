7ec66ead3b8a24084f04fd23edd9633c4c69569d
Magali Semeria <magali.semeria@gmail.com>
2021-02-13 18:43:16 +0100

    Add face detector to detech humans

--- dog_app/__init__.py
+++ dog_app/__init__.py
@@ -1,6 +1,6 @@
+import cv2
 import json
 import os
-
 import torch
 
 from PIL import Image
@@ -18,6 +18,8 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 3 * 1024 * 1024
 
 imagenet_class_index = json.load(open(os.path.join(basedir, '_static/imagenet_class_index.json')))
+# extract pre-trained face detector
+face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt.xml')
 
 model = models.vgg16(pretrained=True)
 custom_model = os.path.join(basedir, '../model/model_transfer.pt')
@@ -89,6 +91,16 @@ def get_prediction(filename):
     class_id, class_name = imagenet_class_index[str(prediction)]
     print(prediction, class_name)
     return class_name.replace("_", " ")
+
+
+def face_detector(img_path):
+    """
+        Return True if face is detected in image stored at img_path
+    """
+    img = cv2.imread(img_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray)
+    return len(faces) > 0
 
 
 @app.route('/')
